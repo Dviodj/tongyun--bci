9d9f0ea2bd7473d4de2a09159dd91862536fd355
Dviodj <2584950136@qq.com>
2026-03-27 20:48:32 +0800

    Final: Traditional method 71.5% beats EEGNet 61.5% - stable results

--- _push.bat
+++ _push.bat
@@ -1,6 +1,6 @@
 @echo off
 cd /d D:\brainwave-morse
 git add -A
-git commit -m "Update: EEGNet pretraining achieves 77.8%% LOSO - BCICIV 2b results"
+git commit -m "Final: Traditional method 71.5%% beats EEGNet 61.5%% - stable results"
 git push origin main
 echo [DONE]
